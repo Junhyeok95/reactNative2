0ae4d19029118ba2aec0106f4a5be98eb4c32975
Junhyeok Jang <btrya23@gmail.com>
2020-10-28 21:55:23 +0900

    KURUMAMORI, img error

--- AwesomeProject/src/App.tsx
+++ AwesomeProject/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, Text } from 'react-native';
+import {SafeAreaView, StatusBar, View, Text, Image} from 'react-native';
 import Home from '~/Screens/Home';
 import Profile from '~/Screens/Profile';
 import SpeechTest from '~/Screens/SpeechTest';
@@ -22,7 +22,29 @@ const App = ({}: Props) => {
       {/* <Mail /> */}
       {/* <Multi /> */}
       {/* <Multi3 /> */}
-      <TTS />
+      {/* <TTS /> */}
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text
+          style={{
+            fontSize: 40,
+            textAlign: 'center',
+            backgroundColor: '#00FF0055',
+          }}>
+          Hello
+        </Text>
+        <View style={{backgroundColor: '#00FF0077'}}>
+          <Image
+            style={{
+              width: 50,
+              height: 50,
+            }}
+            source={{
+              uri: 'https://pbs.twimg.com/media/EGR6H_dXkAE9Pu8.jpg',
+            }}
+            // source={require('./abc.png')}
+          />
+        </View>
+      </View>
     </>
   );
 };
